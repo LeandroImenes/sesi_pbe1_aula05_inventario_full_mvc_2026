ea8ef8b325f8bf5ae8e8a48c21dd52f13fae4140
Seu Nome <Seu E-mail>
2026-09-17 11:25:00 -0300

    oi

--- src/controllers.js
+++ src/controllers.js
@@ -8,11 +8,13 @@ function autoIncrement() {
     return ultimoId + 1;
 }
 
+//cruds
 const create = (req, res) => {
     const dados = req.body;
     dados.id = autoIncrement();
     bens.push(dados);
-    res.status(201).json(dados);
+    // res.status(201).json(dados);
+    res.redirect("http://127.0.0.1:5500/client/index.html")
 };
 
 const read = (req, res) => {
